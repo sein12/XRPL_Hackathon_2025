67a4466b5d9673b89ac2f94be408669119f30110
sein12 <moses1008@naver.com>
2025-09-15 12:46:33 +0900

    fix[BE]: plus User Table "name" col

--- backend/prisma/migrations/20250915034541_init/migration.sql
+++ backend/prisma/migrations/20250915034541_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "passwordHash" TEXT NOT NULL,
     "email" TEXT,
